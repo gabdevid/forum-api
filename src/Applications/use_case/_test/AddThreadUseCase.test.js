7235aab8e2ef3c0399335c11cbd9223d5cfb841e
GAB Developer Indonesia <ghulamasrofibuntoro@gmail.com>
2023-12-14 10:25:35 +0700

    initial commit

--- src/Applications/use_case/_test/AddThreadUseCase.test.js
+++ src/Applications/use_case/_test/AddThreadUseCase.test.js
@@ -1,41 +1,32 @@
 const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
-const AddedThread = require('../../../Domains/threads/entities/AddedThread');
 const AddThreadUseCase = require('../AddThreadUseCase');
 
 describe('AddThreadUseCase', () => {
   it('should orchestrating the add thread action correctly', async () => {
-   
-    // Arrange
     const mockThreadRepository = new ThreadRepository();
-    const mockReturnAddedThread = new AddedThread({
+    const mockReturnAddThread = {
       id: 'thread-123',
-      title: 'title',
+      title: 'sebuah thread',
       owner: 'user-123',
-  })
-
-    mockThreadRepository.addThread = jest.fn(() => Promise.resolve(mockReturnAddedThread));
-
-
+    };
+    mockThreadRepository.addThread = jest.fn(() => Promise.resolve(mockReturnAddThread));
     const useCase = new AddThreadUseCase({
       threadRepository: mockThreadRepository,
     });
     const useCasePayload = {
-      title: 'title',
-      body: 'body',
+      title: 'sebuah thread',
+      body: 'sebuah body thread',
+      owner: 'user-123',
+    };
+    const expectedAddedThread = {
+      id: 'thread-123',
+      title: 'sebuah thread',
       owner: 'user-123',
     };
 
-    const expectedAddedThread = new AddedThread({
-      id: 'thread-123',
-      title: 'title',
-      owner: 'user-123',
-    });
-
-    // Action
     const addedThread = await useCase.execute(useCasePayload);
 
-    // Assert
     expect(addedThread).toStrictEqual(expectedAddedThread);
-    expect(mockThreadRepository.addThread).toBeCalledWith(new AddThread(useCasePayload));
+    expect(mockThreadRepository.addThread).toBeCalledWith(useCasePayload);
   });
 });
